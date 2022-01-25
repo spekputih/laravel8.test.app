899fd30c4b2863f7ac3aec0c0afe76f8c0747736
Afiq Syazwan <fafiqsyaz@gmail.com>
2022-01-25 23:01:55 +0800

    add user database seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        // DB::table('users')->insert([
+        //     'name' => $this->faker->name,
+        //     'email' => $this->faker->unique()->safeEmail,
+        //     'email_verified_at' => now(),
+        //     'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //     'remember_token' => Str::random(10),
+        // ]);
+        User::factory()
+            ->count(50)
+            ->create();
     }
 }
