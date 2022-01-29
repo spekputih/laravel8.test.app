a2e66556f08388562e1b0788dc0c2e47f5c3d4de
Afiq Syazwan <fafiqsyaz@gmail.com>
2022-01-29 16:09:56 +0800

    add admin user and overiding the gate permission

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,6 +28,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'is_admin' => false
         ];
     }
 
@@ -37,7 +38,9 @@ class UserFactory extends Factory
             return [
                 'name' => 'Afiq Syazwan',
                 'email' => 'afiq.syazwan192@gmail.com',
+                'is_admin' => true
                 // 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+
             ];
         });
     }
